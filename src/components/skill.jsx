394403559a9a6148e89327973669259923132613
unknown <radheshyamjha023@gmail.com>
2024-01-22 11:04:53 -0800

    third commit

--- src/components/skill.jsx
+++ src/components/skill.jsx
@@ -1,5 +1,129 @@
-const Skill = ()=>{
-    return <><div className="text-white">this is My skills</div></>
-}
+import "../css/skill.css"
+import html_logo from "../assets/html_logo.png"
+import css_logo from "../assets/CSS3_logo.png"
+import js_logo from "../assets/js_logo.png"
+import python_logo from "../assets/python_logo.png"
+import bootstrap from "../assets/bootstrap_logo.png"
+import tailwind from "../assets/tailwind.png"
+import react_logo from "../assets/react_logo.png"
+import express_logo from "../assets/express_logo.png"
+import node_logo from "../assets/node_logo.png"
+import django_logo from "../assets/django_logo.png"
+import mongoose_logo from "../assets/mongoose_logo.png"
+import firebase from "../assets/firebase_logo.png"
+import mongodb from "../assets/mongo_logo.svg"
+import mysql from "../assets/mysql_logo.png"
+import postgresql from "../assets/postgresql.png"
+import git from "../assets/git_logo.png"
+const Skill = () => {
+    return (
+    <>
+        <div className="mt-12 flex place-content-center text-4xl font-bold text-blue-800">My Skills</div>
+        <div className="flex  place-content-center mb-12">
+            <div className="parent-skill-div rounded-2xl mt-8 flex place-content-center">
+                <div className="grid grid-cols-4 p-6 w-full gap-5">
+                    <div className="child-skill-div rounded-2xl">
+                        <div className=" flex place-content-center pt-6 pb-4">
+                            <img src={html_logo} className="w-5/6 h-auto"/>
+                        </div>
+                        <div className=" flex place-content-center text-cyan-400 text-xl font-semibold pb-3">HTML5</div>
+                    </div>
+                    <div className="child-skill-div rounded-2xl">
+                        <div className=" flex place-content-center pt-6 pb-4">
+                            <img src={css_logo} className="w-5/6 h-auto"/>
+                        </div>
+                        <div className=" flex place-content-center text-cyan-400 text-xl font-semibold pb-3">CSS</div>
+                    </div>
+                    <div className="child-skill-div rounded-2xl">
+                        <div className=" flex place-content-center pt-6 pb-4">
+                            <img src={js_logo} className="w-5/6 h-auto"/>
+                        </div>
+                        <div className=" flex place-content-center text-cyan-400 text-xl font-semibold pb-3">JavaScript</div>
+                    </div>
+                    <div className="child-skill-div rounded-2xl">
+                        <div className=" flex place-content-center pt-6 pb-4">
+                            <img src={python_logo} className="w-5/6 h-auto"/>
+                        </div>
+                        <div className=" flex place-content-center text-cyan-400 text-xl font-semibold pb-3">Python</div>
+                    </div>
+                    <div className="child-skill-div rounded-2xl">
+                        <div className=" flex place-content-center pt-6 pb-4">
+                            <img src={bootstrap} className="w-5/6 h-auto"/>
+                        </div>
+                        <div className=" flex place-content-center text-cyan-400 text-xl font-semibold pb-3">Bootstrap</div>
+                    </div>
+                    <div className="child-skill-div rounded-2xl">
+                        <div className=" flex place-content-center pt-6 pb-4">
+                            <img src={tailwind} className="w-5/6 h-auto"/>
+                        </div>
+                        <div className=" flex place-content-center text-cyan-400 text-xl font-semibold pb-3">Tailwind CSS</div>
+                    </div>
+                    <div className="child-skill-div rounded-2xl">
+                        <div className=" flex place-content-center pt-6 pb-4">
+                            <img src={react_logo} className="w-5/6 h-auto"/>
+                        </div>
+                        <div className=" flex place-content-center text-cyan-400 text-xl font-semibold pb-3">React.js</div>
+                    </div>
+                    <div className="child-skill-div rounded-2xl">
+                        <div className=" flex place-content-center pt-6 pb-4">
+                            <img src={node_logo} className="w-5/6 h-auto"/>
+                        </div>
+                        <div className=" flex place-content-center text-cyan-400 text-xl font-semibold pb-3">Node.js</div>
+                    </div>
+                    <div className="child-skill-div rounded-2xl">
+                        <div className=" flex place-content-center pt-6 pb-4">
+                            <img src={express_logo} className="w-5/6 h-auto"/>
+                        </div>
+                        <div className=" flex place-content-center text-cyan-400 text-xl font-semibold pb-3">Express.js</div>
+                    </div>
+                    <div className="child-skill-div rounded-2xl">
+                        <div className=" flex place-content-center pt-6 pb-4">
+                            <img src={django_logo} className="w-5/6 h-auto"/>
+                        </div>
+                        <div className=" flex place-content-center text-cyan-400 text-xl font-semibold pb-3">Django</div>
+                    </div>
+                    <div className="child-skill-div rounded-2xl">
+                        <div className=" flex place-content-center pt-6 pb-4">
+                            <img src={mongoose_logo} className="w-5/6 h-auto"/>
+                        </div>
+                        <div className=" flex place-content-center text-cyan-400 text-xl font-semibold pb-3">Mongoose</div>
+                    </div>
+                    <div className="child-skill-div rounded-2xl">
+                        <div className=" flex place-content-center pt-6 pb-4">
+                            <img src={mongodb} className="w-5/6 h-auto"/>
+                        </div>
+                        <div className=" flex place-content-center text-cyan-400 text-xl font-semibold pb-3">MongoDB</div>
+                    </div>
+                    <div className="child-skill-div rounded-2xl">
+                        <div className=" flex place-content-center pt-6 pb-4">
+                            <img src={firebase} className="w-5/6 h-auto"/>
+                        </div>
+                        <div className=" flex place-content-center text-cyan-400 text-xl font-semibold pb-3">Firebase</div>
+                    </div>
+                    <div className="child-skill-div rounded-2xl">
+                        <div className=" flex place-content-center pt-6 pb-4">
+                            <img src={mysql} className="w-5/6 h-auto"/>
+                        </div>
+                        <div className=" flex place-content-center text-cyan-400 text-xl font-semibold pb-3">MySQL</div>
+                    </div>
+                    <div className="child-skill-div rounded-2xl">
+                        <div className=" flex place-content-center pt-6 pb-4">
+                            <img src={postgresql} className="w-5/6 h-auto"/>
+                        </div>
+                        <div className=" flex place-content-center text-cyan-400 text-xl font-semibold pb-3">PostgreSQL</div>
+                    </div>
+                    <div className="child-skill-div rounded-2xl">
+                        <div className=" flex place-content-center pt-6 pb-4">
+                            <img src={git} className="w-5/6 h-auto"/>
+                        </div>
+                        <div className=" flex place-content-center text-cyan-400 text-xl font-semibold pb-3">Git</div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </>
+    );
+};
 
 export default Skill;
